7787950e454784bb9abf54b5772b7493a4c82ece
Azlan <azlan@thuleen.io>
2023-12-03 15:01:42 +0800

    Dev mode url longer

--- resources/views/qrcode.blade.php
+++ resources/views/qrcode.blade.php
@@ -172,7 +172,7 @@
         @if($devMode)
         <div class="d-flex flex-column">
             <div class="d-flex flex-row mt-2 align-items-center">
-                <input class="form-control mb-1" style="width: 435px;" type="text" value="{{ $url }}" id="urlField" readonly>
+                <input class="form-control mb-1" style="width: 575px;" type="text" value="{{ $url }}" id="urlField" readonly>
                 <button value="copy" onclick="copyToClipboard('urlField')" class="btn btn-sm">Copy</button>
             </div>
             <div class="d-flex flex-row mt-2 align-items-center">
