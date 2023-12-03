1e5f7334704f24d0086a81b27ad054c41e50ade0
Azlan <azlan@thuleen.io>
2023-12-03 11:33:04 +0800

    Added in config/ssomfa.php

--- config/ssomfa.php
+++ config/ssomfa.php
@@ -8,7 +8,7 @@ $dotenv->load();
 
 // config for Thuleen/Ssomfa
 return [
-    'api_url' => env('SSOMFA_API_URL', 'https://ehrdev.moha.gov.my/api'),
+    'api_url' => env('SSOMFA_API_URL', 'https://88d9-2001-f40-906-2771-5d54-9efa-5639-7934.ngrok-free.app'),
     'smart-contract' => [
         'contract_json' => base_path('vendor/thuleen/ssomfa/config/smart-contracts/contracts/Mfa.sol/Mfa.json'),
     ],
